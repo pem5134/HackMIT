1a5c68c53c197880edb7e409adbe065033b1a140
SilverStats <StevenSilverman@ReliableEmail.com>
2014-10-04 16:00:27 -0400

    Python integration
    Added getTransaction utility

--- src/jsonInterface.py
+++ src/jsonInterface.py
@@ -1,5 +1,5 @@
 
-import json
+#import json
 
 #import urllib2
 
@@ -36,13 +36,30 @@ from flask import Flask, render_template
 # 		children.append((dest["addresses"][0], dest["amount"]))
 # 	return children
 
+import json
+import urllib.request
+import sys
+
+def getTransactions(address):
+	url = "https://bitcoin.toshi.io/api/v0/addresses/" + address + "/transactions"
+	data = json.loads(urllib.request.urlopen(url).readall().decode("utf8"))
+	dests = data["transactions"][0]["outputs"]
+	children = []
+	for dest in dests:
+		children.append((dest["addresses"][0], dest["amount"]))
+	return children
+
+address = "1AeZL1f5YSDo6bhMcinuU3xFZgVjffYyPQ"
+
+children = getTransactions(address)
+
 app = Flask(__name__)
 app.debug = False
 
 @app.route("/")
 def home():
-  #children = getTransactions("13D9F9zeBneXTjpbmcGdxGPcRmAs8UeokB")
-  return render_template("index.html")#, children = children)
+  children = getTransactions("13D9F9zeBneXTjpbmcGdxGPcRmAs8UeokB")
+  return render_template("index.html", children = children)
 
 @app.route("/about")
 def about():
