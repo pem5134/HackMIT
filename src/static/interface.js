93c1972329c6d3bc76b7bbe58db67de92bd6db1d
SilverStats <StevenSilverman@ReliableEmail.com>
2014-10-04 23:52:09 -0400

    new interface

--- src/static/interface.js
+++ src/static/interface.js
@@ -5,8 +5,9 @@
 
 function on_request_success(response) {
 	var data = JSON.parse(response);
+	console.log(data)
 	//console.log(response);
-	console.log(data["transactions"][0]["outputs"][0]["addresses"][0]);
+	//console.log(data["transactions"][0]["outputs"][0]["addresses"][0]);
 }
 
 $.ajax({
@@ -16,3 +17,21 @@ $.ajax({
 	success: on_request_success
 }
 );
+
+function createGraph(address) {
+	$.getJSON('http://localhost:5000/_getData', {
+		word: address
+	}, function(data){
+		console.log(data.result)
+		$( "#result" ).text(data.result);
+	});
+}
+
+function sendToPython(list) {
+	$.getJSON('http://localhost:5000/_array2python', {
+        wordlist: JSON.stringify(list)
+    }, function(data){
+        console.log(data.result)
+        $( "#result" ).text(data.result);
+    });
+}
